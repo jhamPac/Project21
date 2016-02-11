4aab885dc0b23cbc3dd698dde38195888f839a96
Kevia <cloud@kevia.me>
2016-02-11 13:20:58 -0800

    uicollection done

--- P2P/P2P/ViewController.swift
+++ P2P/P2P/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate
 {
+    var images = [UIImage]()
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet{
+            collectionView.dataSource = self
+            collectionView.delegate = self
+        }
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -21,7 +30,58 @@ class ViewController: UIViewController
     
     func importPicture()
     {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    //MARK: - CollectionView Protocols
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageView", forIndexPath: indexPath)
         
+        if let imageView = cell.viewWithTag(1000) as? UIImageView
+        {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
+    
+    //MARK: - UIImagePicker Protocols
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        var newImage: UIImage
+        
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage
+        {
+            newImage = possibleImage
+        }
+        else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage
+        {
+            newImage = possibleImage
+        }
+        else
+        {
+            return
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        images.insert(newImage, atIndex: 0)
+        collectionView.reloadData()
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
